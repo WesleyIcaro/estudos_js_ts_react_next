c65e5bd2501406d7c6d2b87c4aea3d375fdcd6bc
Sturgart <wesleyicaro4@gmail.com>
2023-03-29 00:34:14 -0300

    Unknow Commit

---
+++ aulas/aula16/index.js
@@ -0,0 +1,35 @@
+//            01234567891011
+const nome = 'Wesley Icaro'
+
+let alunos = ['Luiz', 'Maria', 'João'] // Luana, Victor
+alunos = 544
+
+console.log(typeof alunos) // Vai passar a informação que é um object
+
+console.log(alunos instanceof Array) // Se dar "True" é realmente um array
+
+// console.log(alunos.slice(0, -2)) // Do começo até o antepenúltimo
+
+// console.log(alunos.slice(0, 3)) // Mostra os 3 primeiros do array
+
+// delete alunos[1] // remove o conteúdo do indíce indicado, mas mantém a existência do indíce sem  alterar os demais.
+
+// alunos.unshift('Lidio') // Adiciona no inicio
+// alunos.unshift('Tamara') // Adiciona no inicio
+
+// const removidos = alunos.pop() // remove do final
+
+// const removidos = alunos.shift() // remove do começo
+
+// console.log(removidos)
+
+// alunos.push('Lidio') // Adiciona no fim
+// alunos.push('Tamara') // Adiciona no fim
+
+// alunos[0] = 'Gabriel' // Altera
+// alunos[3] = 'Gabriel' // Adiciona
+
+// alunos[alunos.length] = 'Guilhermino' // Adiciona no fim
+// alunos[alunos.length] = 'Vagner' // Adiciona no fim
+// alunos[alunos.length] = 'Lulu' // Adiciona no fim
+// alunos[alunos.length] = 'VDelas' // Adiciona no fim
---
+++ aulas/aula17/index.js
@@ -0,0 +1,25 @@
+function saudacao(nome) {
+  return `${nome}, bom dia!` // Utilizar o return é importante porque envia o valor que a função retorna para a váriavel
+}
+
+const variavel = saudacao('Wesley')
+console.log(variavel)
+
+function soma(x = 10, y = 56) {
+  const somando = x + y
+  return somando
+}
+
+// console.log(soma(0, 7))
+
+const raiz = function (n) {
+  const conta = n ** 0.5
+  return conta
+}
+
+// console.log(raiz(9))
+
+const resto = (n, r) => n % r
+
+const restante = resto(13, 2)
+console.log(restante)
