c66ec89aad8f159034a753f64ea567821f9abffe
Sturgart <wesleyicaro4@gmail.com>
2023-04-09 21:52:20 -0300

    Commit let, var

--- aulas/aula29/JS/index.js
+++ aulas/aula29/JS/index.js
@@ -1,23 +1,73 @@
-const data = new Date('1987-8-26 00:00:00')
+function getDayWeekText(weekDay) {
+  switch (weekDay) {
+    case 0:
+      return (weekDayTextweekDayText = 'Domingo')
+    case 1:
+      return (weekDayTextweekDayText = 'Segunda')
+    case 2:
+      return (weekDayText = 'Terça-feira')
+    case 3:
+      return (weekDayText = 'Quarta-feira')
+    case 4:
+      return (weekDayText = 'Quinta-feira')
+    case 5:
+      return (weekDayText = 'Sexta-feira')
+    case 6:
+      return (weekDayText = 'Sábado')
+    default:
+      return (weekDayText = 'Dia da semana não encontrado')
+  }
+}
+
+const data = new Date('2023-04-10 00:00:00')
 
 const weekDay = data.getDay()
 
 let weekDayText
 
-if (weekDay === 0) {
-  weekDayText = 'Domingo'
-} else if (weekDay === 1) {
-  weekDayText = 'Segunda'
-} else if (weekDay === 2) {
-  weekDayText = 'Terça-feira'
-} else if (weekDay === 3) {
-  weekDayText = 'Quarta-feira'
-} else if (weekDay === 4) {
-  weekDayText = 'Quinta-feira'
-} else if (weekDay === 5) {
-  weekDayText = 'Sexta-feira'
-} else {
-  weekDayText = 'Sabádo'
-}
+console.log(weekDay, getDayWeekText(weekDay))
 
-console.log(weekDay, weekDayText)
+// switch (weekDay) {
+//   case 0:
+//     weekDayText = 'Domingo'
+//     break
+//   case 1:
+//     weekDayText = 'Segunda'
+//     break
+//   case 2:
+//     weekDayText = 'Terça-feira'
+//     break
+//   case 3:
+//     weekDayText = 'Quarta-feira'
+//     break
+//   case 4:
+//     weekDayText = 'Quinta-feira'
+//     break
+//   case 5:
+//     weekDayText = 'Sexta-feira'
+//     break
+//   case 6:
+//     weekDayText = 'Sábado'
+//     break
+//   default:
+//     weekDayText = 'Dia da semana não encontrado'
+//     break
+// }
+
+// if (weekDay === 0) {
+//   weekDayText = 'Domingo'
+// } else if (weekDay === 1) {
+//   weekDayText = 'Segunda'
+// } else if (weekDay === 2) {
+//   weekDayText = 'Terça-feira'
+// } else if (weekDay === 3) {
+//   weekDayText = 'Quarta-feira'
+// } else if (weekDay === 4) {
+//   weekDayText = 'Quinta-feira'
+// } else if (weekDay === 5) {
+//   weekDayText = 'Sexta-feira'
+// } else if (weekDay === 6) {
+//   weekDayText = 'Sabádo'
+// } else {
+//   weekDayText = 'Dia da semana não encontrado.'
+// }
---
+++ aulas/aula31/JS/index.js
@@ -0,0 +1,35 @@
+const verdade = true
+
+// // Let tem escopo de bloco { ...bloco }
+// // Var só tem escopo de função
+
+// let nome = 'Luiz' // criando
+// var nome2 = 'Luiz' // criando
+
+// if (verdade) {
+//   let nome = 'Otávio' // criando
+//   var nome2 = 'Otárrio' // redeclarando
+//   // console.log(nome, nome2)
+//   if (verdade) {
+//     var nome2 = 'Otário' // redeclarando
+//     let nome = 'Outro nome'
+//     console.log(nome, nome2)
+//   }
+// }
+
+// function falaOi() {
+//   if (verdade) {
+//     let nome = 'luizinhu'
+//     var sobrenome = 'Lulu'
+//   }
+
+//   console.log(sobrenome)
+// }
+
+// // console.log(sobrenome)
+
+// falaOi()
+
+console.log(sobrenome)
+
+var sobrenome = 'Miranda'
