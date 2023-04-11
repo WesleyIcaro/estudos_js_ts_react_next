a6fd893485f4c537b58ded9542aef2f423ebc839
Sturgart <wesleyicaro4@gmail.com>
2023-04-11 01:28:38 -0300

    Commit initial yet

--- aulas/aula32/JS/index.js
+++ aulas/aula32/JS/index.js
@@ -6,4 +6,20 @@ const letras = [b, c, a]
 
 ;[a, b, c] = letras
 
-console.log(a, b, c)
+// console.log(a, b, c)
+
+// ...rest, ...spread
+const numeros = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+const [um, , tres, , cinco, , sete, oito] = numeros
+
+console.log(um, tres, cinco, sete, oito)
+
+const numbers = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+const [, , [, , nove]] = numbers
+
+console.log(nove)
---
+++ aulas/aula33/index.js
@@ -0,0 +1,10 @@
+const pessoa = {
+  nome: 'Wesley',
+  Sobrenome: 'Icaro',
+  idade: 24,
+  endereco: {
+    rua: 'Jornalista Milton Cury',
+    numero: 21
+  }
+}
+console.log(pessoa)
