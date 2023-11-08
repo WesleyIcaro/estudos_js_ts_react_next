eeb707d162106999dfd589597877b288adef6937
21161408-5 <wesleyicaro4@gmail.com>
2023-11-08 00:30:26 -0300

    New class.

--- aulas/aula70/aula70.js
+++ aulas/aula70/aula70.js
@@ -1,38 +1,75 @@
 const pessoa = {
-  nome: 'Wesley',
-  sobrenome: 'Icaro',
-  idade: 25
-}
+  nome: "Wesley",
+  sobrenome: "Icaro",
+  idade: 25,
+};
 
-const chave = 'nome'
+const chave = "nome";
 // console.log(pessoa[chave])
 // console.log(pessoa.sobrenome)
 // console.log(pessoa['idade'])
 
-const pessoa1 = new Object()
-pessoa1.nome = 'José'
-pessoa1.sobrenome = 'Maria'
-pessoa1.idade = 25
+const pessoa1 = new Object();
+pessoa1.nome = "José";
+pessoa1.sobrenome = "Maria";
+pessoa1.idade = 25;
 
 // console.log(pessoa1[chave])
 // console.log(pessoa1['sobrenome'])
 
-delete pessoa1[chave]
+delete pessoa1[chave];
 // console.log(pessoa1)
 
 pessoa1.falarNome = function () {
-  console.log(`${this.sobrenome} está falando o seu nome.`)
-}
+  return `${this.sobrenome} está falando o seu nome.`;
+};
 
 pessoa1.getDataNascimento = function () {
-  const dataAtual = new Date()
-  return dataAtual.getFullYear() - this.idade
-}
+  const dataAtual = new Date();
+  return dataAtual.getFullYear() - this.idade;
+};
 
 // console.log(pessoa1.getDataNascimento())
 
 for (let chave in pessoa1) {
-  console.log(pessoa1[chave])
+  console.log(pessoa1[chave]);
 }
 
 // Functions em objetos são métodos
+function criarPessoa(nomeAbencoado, sobrenomeAbencoado) {
+  return {
+    nomeAbencoado,
+    sobrenomeAbencoado,
+    nomeCompletoAbencoado() {
+      return `${this.nomeAbencoado} ${this.sobrenomeAbencoado}`;
+    },
+  };
+}
+
+const p1 = criarPessoa("Wesley", "Icaro");
+
+console.log(p1.nomeCompletoAbencoado());
+
+function Carro(marca, ano) {
+  this.marca = marca;
+  this.ano = ano;
+
+  Object.freeze(this);
+
+  // return this;
+}
+
+// p1 = (ENDERECOMEMORIA) -> "Valor"
+// P1.ENDERECOMEMORIA =  {marca: 'Outra coisa}
+// p1 = (NOVOENDERECOMEMORIA) -> ""
+const c1 = new Carro("Fiat", 2023);
+delete c1.marca;
+c1.ano = 2013;
+const c2 = new Carro("Ford", 2018);
+console.log(c1);
+
+c2.fala = function () {
+  console.log("Olá mundo!!!");
+};
+
+// c2.fala();
