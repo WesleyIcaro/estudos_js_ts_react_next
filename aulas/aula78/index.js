f66deb22eed232a8dd9ba48dbacfa19203f9d1a6
WesleyIcaro <wesleyicaro4@gmail.com>
2024-01-26 17:20:38 -0300

    New class.

--- aulas/aula78/index.js
+++ aulas/aula78/index.js
@@ -7,7 +7,7 @@ function Conta(agencia, conta, saldo) {
 
 Conta.prototype.sacar = function (valor) {
     if (valor > this.saldo) {
-        console.log(`Saldo insuficiente: ${this.saldo}`)
+        console.log(`Saldo insuficiente: R$ ${this.saldo}`)
         return
     };
 
@@ -23,13 +23,42 @@ Conta.prototype.depositar = function (valor) {
 Conta.prototype.verSaldo = function () {
     console.log(
         `Ag/c: ${this.agencia}/${this.conta} | ` +
-        `Saldo: R$${this.saldo.toFixed(2)}`
+        `Saldo: R$ ${this.saldo.toFixed(2)}`
     )
 }
 
-const conta1 = new Conta("2404", "0044998", 10)
-// console.log(conta1)
-conta1.depositar(11)
-conta1.depositar(10)
-conta1.sacar(30)
-conta1.sacar(0.01)
+const conta1 = new Conta("2405", "0174988", 10)
+// conta1.depositar(11)
+// conta1.depositar(10)
+// conta1.sacar(30)
+// conta1.sacar(0.87)
+
+function ContaCorrente(agencia, conta, saldo, limite) {
+    Conta.call(this, agencia, conta, saldo)
+    this.limite = limite;
+}
+
+ContaCorrente.prototype = Object.create(Conta.prototype)
+ContaCorrente.prototype.constructor = ContaCorrente
+
+ContaCorrente.prototype.sacar = function (valor) {
+    if (valor > this.saldo + this.limite) {
+        console.log(`Saldo insuficiente: R$ ${this.saldo}`)
+        return
+    };
+
+    this.saldo -= valor
+    this.verSaldo()
+}
+
+function ContaPoupanca(agencia, conta, saldo) {
+    Conta.call(this, agencia, conta, saldo)
+}
+
+ContaPoupanca.prototype = Object.create(Conta.prototype)
+ContaPoupanca.prototype.constructor = ContaCorrente
+
+const conta2 = new ContaPoupanca(12, 33, 0)
+conta2.depositar(10)
+conta2.sacar(10)
+conta2.sacar(1)
