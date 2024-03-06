68edd7025e0e4ab46658972aa67e0ae3168a733b
WesleyIcaro <wesleyicaro4@gmail.com>
2024-03-06 15:41:44 -0300

    New class.

--- aulas/aula92Babel/main.js
+++ aulas/aula92Babel/main.js
@@ -1,1 +1,14 @@
-const nome = 'Luiz'
+class Pessoa {
+    constructor(nome, idade) {
+        this.nome = nome
+        this.idade = idade
+
+        this.nomeIdade()
+    }
+
+    nomeIdade() { console.log("Meu nome é ", this.nome, ", e tenho ", this.idade, " anos!") }
+}
+
+const pessoa = new Pessoa("Wesley", 25)
+
+// pessoa.teste()
