0ab35f0517232120a960b4db81d3e253f466482e
Wesley Icaro Lima dos Santos <wesleyicaro4@gmail.com>
2024-03-13 19:47:13 -0300

    New class.

---
+++ aulas/aula94ImportExport/src/index.js
@@ -0,0 +1,22 @@
+class Floresta {
+
+    constructor(arvore, rio) {
+        this.arvore = arvore;
+        this.rio = rio;
+
+        this.aventura()
+    }
+
+    aventura() {
+        console.log(`Na floresta tem ${this.arvore} e o ${this.rio}!`)
+    }
+}
+
+const amazonia = new Floresta("árvore Brasil", "rio amazonas")
+
+// dsfdfsdfdsf
+// dsfdsf
+// fdsfsd
+// fsadfsd
+// asfsad sd
+// fsd
