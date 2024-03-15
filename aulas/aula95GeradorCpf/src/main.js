effd4dd6f12db5fad8a95bf09a0499789516a6e6
Wesley Icaro Lima dos Santos <wesleyicaro4@gmail.com>
2024-03-15 17:31:09 -0300

    New class gerador cpf.

--- aulas/aula95GeradorCpf/src/main.js
+++ aulas/aula95GeradorCpf/src/main.js
@@ -4,6 +4,7 @@ import "./assets/css/style.css";
 
 (function () {
     const gera = new GeraCPF()
+
     const cpfGerado = document.querySelector(".cpf-gerado")
     cpfGerado.innerHTML = gera.geraNovoCpf()
 })();
